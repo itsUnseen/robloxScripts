cfad2e072491392602f36c9bac0c0bcd78e011aa
itsnotalimo <97067810+itsnotalimo@users.noreply.github.com>
2022-02-06 23:21:37 -0600

    Update environmentFunctions.lua

--- environmentFunctions.lua
+++ environmentFunctions.lua
@@ -1,73 +1,74 @@
-local env = {}
+    local env = {}
 
---// SERVICES
-local replicatedStoreage = game.ReplicatedStorage
+    --// SERVICES
+    local replicatedStoreage = game.ReplicatedStorage
 
-local tweenService = game:GetService("TweenService")
-local runService = game:GetService("RunService")
+    local tweenService = game:GetService("TweenService")
+    local runService = game:GetService("RunService")
 
---// VARIABLES
-local player = game.Players.LocalPlayer
-local char = player.Character
-local root = char.PrimaryPart
+    --// VARIABLES
+    local player = game.Players.LocalPlayer
+    local char = player.Character
+    local root = char.PrimaryPart
+    local camera = workspace.CurrentCamera
 
-function env:Distance(P1, P2)
-    if (type(P1) ~= "Vector3") then P1 = P1.Position end
-    if (type(P2) ~= "Vector3") then P2 = P2.Position end
-    
-    return (P1 - P2).magnitude
-end
-
-function env:FindClosestPlayerToMouse(x,y)
-    local closestPlayer = nil
-    local maxDistance = math.huge
-
-    for i,v in pairs (game.Players:GetPlayers()) do
-
-        if (v ~= player and v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Head")) then
-            local worldPointPos = camera:WorldToViewportPoint(v.Character.PrimaryPart.Position)
-            local magnitude = (Vector2.new(worldPointPos.X, worldPointPos.Y) - Vector2.new(x,y)).magnitude
-
-            if (magnitude < maxDistance) then
-                closestPlayer = v
-                maxDistance = magnitude
-            end
-        end
+    function env:Distance(P1, P2)
+        if (type(P1) ~= "Vector3") then P1 = P1.Position end
+        if (type(P2) ~= "Vector3") then P2 = P2.Position end
+        
+        return (P1 - P2).magnitude
     end
-end
 
-function env:Lerp(num, endNum, t, d)
-    return num + (endNum - num) * (t / (d or 100))
-end
+    function env:FindClosestPlayerToMouse(x,y)
+        local closestPlayer = nil
+        local maxDistance = math.huge
 
-function env:FindClosestPart(P1, P2, P3, P4)
-    local holder = {}
-    local startPart, partName, partType = P1, P2, P3
-    local maxDis = P4
-    local obj = nil
+        for i,v in pairs (game.Players:GetPlayers()) do
 
-    if (typeof(partName) == 'table') then
-        for i,v in pairs (workspace:GetDescendants()) do
-            if (table.find(partName, v.Name) and v:IsA(partType)) then
-                table.insert(holder, v)
-            end
-        end
-    else
-        for i,v in pairs (workspace:GetDescendants()) do
-            if (v.Name == partName and v:IsA(partType)) then
-                table.insert(holder, v)
+            if (v ~= player and v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Head")) then
+                local worldPointPos = camera:WorldToViewportPoint(v.Character.PrimaryPart.Position)
+                local magnitude = (Vector2.new(worldPointPos.X, worldPointPos.Y) - Vector2.new(x,y)).magnitude
+
+                if (magnitude < maxDistance) then
+                    closestPlayer = v
+                    maxDistance = magnitude
+                end
             end
         end
     end
 
-    for i,v in pairs (holder) do
-        if (env:Distance(v, startPart) < maxDis) then
-            maxDis = env:Distance(v, startPart)
-            obj = v
-        end
+    function env:Lerp(num, endNum, t, d)
+        return num + (endNum - num) * (t / (d or 100))
     end
 
-    return obj
-end
+    function env:FindClosestPart(P1, P2, P3, P4)
+        local holder = {}
+        local startPart, partName, partType = P1, P2, P3
+        local maxDis = P4
+        local obj = nil
 
-return env
+        if (typeof(partName) == 'table') then
+            for i,v in pairs (workspace:GetDescendants()) do
+                if (table.find(partName, v.Name) and v:IsA(partType)) then
+                    table.insert(holder, v)
+                end
+            end
+        else
+            for i,v in pairs (workspace:GetDescendants()) do
+                if (v.Name == partName and v:IsA(partType)) then
+                    table.insert(holder, v)
+                end
+            end
+        end
+
+        for i,v in pairs (holder) do
+            if (env:Distance(v, startPart) < maxDis) then
+                maxDis = env:Distance(v, startPart)
+                obj = v
+            end
+        end
+
+        return obj
+    end
+
+    return env
